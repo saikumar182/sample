428b928b87372ae7c8c4c4c4e533fc55a5856f1e
saikumar182 <129929094+saikumar182@users.noreply.github.com>
2023-04-05 11:07:00 +0530

    Add files via upload

---
+++ SpringClientDemoApplication.java
@@ -0,0 +1,13 @@
+//package com.img2.spring.client.demo;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SpringClientDemoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringClientDemoApplication.class, args);
+//	}
+//
+//}
